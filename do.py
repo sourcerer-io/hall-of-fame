69883acb27e3bf11b09913ce9c11abf8a6855bc1
Sergey Surkov <sergey@sourcerer.io>
2018-07-16 22:22:09 -0700

    feat: Basic implementation of GitHub repo tracking.

--- do.py
+++ do.py
@@ -2,7 +2,7 @@
 
 Commands
     add: Adds a repo to track.
-    delete: Deletes a tracked repo.
+    remove: Deletes a tracked repo.
     list: Lists all tracked repos.
     update: Updates a repo.
 """
@@ -11,34 +11,63 @@ __copyright__ = '2018 Sourcerer, Inc.'
 __author__ = 'Sergey Surkov'
 
 import argparse
+import os.path
+
+from fame.github_tracker import RepoTracker, TrackerError
+
+
+def is_repo_command(command):
+    return command in ['add', 'remove', 'update']
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('command', type=str,
-                        choices=['add', 'delete', 'list', 'update'],
+                        choices=['add', 'remove', 'list', 'update'],
                         help='Command to execute')
-    parser.add_argument('--manager', type=str,
+    parser.add_argument('--user', type=str,
                         help='Github user that tracks a repo')
     parser.add_argument('--owner', type=str,
                         help='Github user that owns a repo')
     parser.add_argument('--repo', type=str,
                         help='Github repo name, excluding owner')
+    parser.add_argument('--work_dir', type=str, required=True,
+                        help='Working directory to store data')
     args = parser.parse_args()
 
-    if args.command in ['add', 'delete', 'update']:
-        if not args.manager:
-            parser.error('Must provide repo manager')
+    if is_repo_command(args.command):
+        if not args.user:
+            parser.error('Must provide repo user')
         if not args.owner:
             parser.error('Must provide repo owner')
         if not args.repo:
             parser.error('Must provide repo name')
+
+    if not os.path.isdir(args.work_dir):
+        parser.error('--work_dir must be an existing directory')
 
     return args
 
 
 def main():
     args = parse_args()
+
+    tracker = RepoTracker(args.work_dir)
+    if is_repo_command(args.command):
+        tracker.configure(args.user, args.owner, args.repo)
+
+    try:
+        if args.command == 'add':
+            tracker.add()
+        elif args.command == 'remove':
+            tracker.remove()
+        elif args.command == 'update':
+            tracker.update()
+        elif args.command == 'list':
+            for user, owner, repo in tracker.list():
+                print('%s:%s/%s' % (user, owner, repo))
+    except TrackerError as e:
+        print('e %s' % str(e))
 
 
 if __name__ == '__main__':
---
+++ fame/github_tracker.py
@@ -0,0 +1,132 @@
+"""GitHub repo tracker tracks commits in the last 7 days."""
+
+__author__ = 'Sergey Surkov'
+__copyright__ = '2018 Sourcerer, Inc'
+
+import os
+from datetime import datetime, timezone
+from os import path
+
+import dateparser
+from google.protobuf import text_format
+from github import Github
+
+from . import github_repo_pb2 as proto
+
+
+class TrackerError(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+
+
+class RepoTracker:
+    def __init__(self, work_dir):
+        self.work_dir = work_dir
+
+    def configure(self, user, owner, repo):
+        """Sets tracker to a repo.
+
+        Args:
+          user: GitHub user who set up tracking for this repo.
+          owner: GitHub repo owner.
+          repo: GitHub repo, repo URL is github.com/:owner/:repo.
+        """
+        self.user = user
+        self.owner = owner
+        self.repo = repo
+
+    def error(self, message):
+        raise TrackerError(
+            '%s %s:%s/%s' % (message, self.user, self.owner, self.repo))
+
+    def add(self):
+        """Adds a repo to track.
+
+        """
+        owner_dir = path.join(self.work_dir, self.user, self.owner)
+        os.makedirs(owner_dir, exist_ok=True)
+        repo = proto.Repo(
+            owner=self.owner, name=self.repo, user=self.user)
+
+        repo_path = path.join(owner_dir, self.repo)
+        if path.exists(repo_path):
+            self.error('Repo exists')
+        with open(path.join(owner_dir, self.repo), 'w') as f:
+            f.write(text_format.MessageToString(repo))
+        print('i Added repo %s:%s/%s' % (self.user, self.owner, self.repo))
+
+    def remove(self):
+        """Removes GitHub repo from tracking."""
+        user_dir = path.join(self.work_dir, self.user)
+        owner_dir = path.join(user_dir, self.owner)
+        repo_path = path.join(owner_dir, self.repo)
+        if path.isfile(repo_path):
+            os.remove(repo_path)
+        else:
+            self.error('Repo not found')
+        if not os.listdir(owner_dir):
+            os.rmdir(owner_dir)
+        if not os.listdir(user_dir):
+            os.rmdir(user_dir)
+        print('i Removed repo %s:%s/%s' % (self.user, self.owner, self.repo))
+
+    def list(self):
+        """Returns all tracked GitHub repos."""
+        for user in os.listdir(self.work_dir):
+            user_dir = path.join(self.work_dir, user)
+            for owner in os.listdir(user_dir):
+                owner_dir = path.join(user_dir, owner)
+                for repo in os.listdir(owner_dir):
+                    yield user, owner, repo
+
+    def load(self):
+        repo_path = path.join(self.work_dir, self.user, self.owner, self.repo)
+        if not path.exists(repo_path):
+            self.error('Repo not found')
+        with open(repo_path) as f:
+            repo = proto.Repo()
+            text_format.Merge(f.read(), repo)
+
+        return repo
+
+    def save(self, repo):
+        repo_path = path.join(self.work_dir, self.user, self.owner, self.repo)
+        with open(repo_path, 'w') as f:
+            f.write(text_format.MessageToString(repo))
+
+
+    def update(self):
+        repo = self.load()
+        last_known = repo.commits[0].sha if repo.recent_commits else None
+
+        github = Github()  # TODO(sergey): Provide user's token here.
+        gh_repo = github.get_repo('%s/%s' % (self.owner, self.repo)) 
+
+        # Get latest commits.
+        commits = []
+        now = datetime.now(timezone.utc)
+        for c in gh_repo.get_commits():
+            if c.sha == last_known:
+                break
+
+            commit = proto.Commit(
+                sha=c.sha, timestamp=c.last_modified, username=c.author.login)
+            commits.append(commit)
+
+            last_modified = dateparser.parse(c.last_modified)
+            if (now - last_modified).days >= 7:
+                # We don't need anything older than one week.
+                break
+
+        # We need to keep just last week's worth of commits.
+        commits.extend(repo.recent_commits)
+        while len(commits) > 1:  # We always want at least one commit.
+            timestamp = dateparser.parse(commits[-1].timestamp) 
+            if (now - timestamp).days < 7:
+                break
+            commits.pop()
+
+        while repo.recent_commits:
+            repo.recent_commits.pop()
+        repo.recent_commits.extend(commits)
+        self.save(repo)

5b5147a1cc2571e122e1a7a33b6d8795dd0d3c1a
Sergey Surkov <sergey@sourcerer.io>
2018-07-28 20:29:58 -0700

    wip: Python path extension for imports. Terrible!

--- cloud/main.py
+++ cloud/main.py
@@ -3,5 +3,17 @@
 __copyright__ = '2018 Sourcerer, Inc.'
 __author__ = 'Sergey Surkov'
 
+import os
+import sys
+from os.path import abspath, dirname
+
+# Obnoxious. TODO(sergey): Find a better way.
+BASE_DIR = dirname(dirname(abspath(__file__)))
+sys.path.append(BASE_DIR)
+
+import fame.storage
+
 def glorify(data, context):
     print('Glory!')
+
+glorify(None, None)
